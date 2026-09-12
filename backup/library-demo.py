311b3ae96ac10ad340ca8db5c171be825157c426
higenobu <33110183+higenobu@users.noreply.github.com>
2026-09-12 11:21:30 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- backup/library-demo.py
+++ backup/library-demo.py
@@ -17,7 +17,7 @@ import dash
 # Config
 # -----------------------------------------------------------------------------
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", "postgresql://matsuo:masanobu@localhost:5432/emr_sample"
+    "DATABASE_URL", "postgresql://xxx:xxxxu@localhost:5432/emr_sample"
 )
 SECRET_KEY = os.environ.get("FLASK_SECRET", "dev-secret-change-me")
 
